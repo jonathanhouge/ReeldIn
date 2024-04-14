79ffeccd8069f7ce100ce9a16a18d3fb9fba0127
jonathanhouge <jonathandhouge@gmail.com>
2024-04-14 15:24:25 -0700

    some triggers removed

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from recommendations.models import Movie, Recommendation
+from recommendations.models import Movie
 
 
 # Create your models here.
