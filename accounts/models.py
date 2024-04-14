9c5b824a0b32af6b7c99aaf67e20318bb45761cc
jonathanhouge <jonathandhouge@gmail.com>
2024-04-14 00:42:00 -0700

    start of working with models, wip

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from recommendations.models import Movie
+from recommendations.models import Movie, Recommendation
 
 
 # Create your models here.
--- recommendations/views.py
+++ recommendations/views.py
@@ -1,24 +1,54 @@
 from django.shortcuts import render
 from .forms import *
-from .models import Recommendation
+from .models import Movie, Recommendation
+import random
 
-# TODO make this short and concise and move step-by-step (this is JUST for step 1 -> step 2 right now)
+FORMS = [GenreForm(), YearForm()]
+FIELD = ["Genres"]
+
+
 def recommend_view(request):
-    form = GenreForm(request.POST)
-    if form.is_valid():
-        selected_genres = form.cleaned_data.get("Genres", [])
-        # Recommendation(genres=selected_genres)
-        # Recommendation.save()
+    form = GenreForm(request.POST)  # TODO generic
 
-    form = YearForm()
-    return render(
-        request,
-        "recommendations/index.html",
-        {"form": form},
-    )
+    # keep track of for logged in user, give a random joe any ol' form next (experience w/o actual rec)
+    if form.is_valid() and request.user.is_authenticated:
+        recommendation = Recommendation.objects.filter(id=request.user)
+        step = recommendation.step
+        selection = form.cleaned_data.get(FIELD[step], [])
+
+        movies = []
+        if step == 0:
+            movies = Movie.objects.filter(genres=selection)
+        else:
+            movies = recommendation.movies.filter(field=selection)  # TODO generic
+
+        recommendation.movies = movies
+        recommendation.possible_film_count = len(movies)
+        recommendation.step += 1
+        recommendation.genres = selection  # TODO generic
+
+        Recommendation.save()
+
+        form = FORMS[step + 1]
+        return render(
+            request,
+            "recommendations/index.html",
+            {"form": form},
+        )
+    elif form.is_valid() and request.user.is_athenticated is False:
+        form = FORMS[random.randint(0, len(FORMS))]
+        return render(
+            request,
+            "recommendations/index.html",
+            {"form": form},
+        )
+
+    # form was invalid
+    return render(request, "dashboard/404.html")  # TODO implement punishment
 
 
 def index(request):
+    print(request.user.is_authenticated)
     form = GenreForm()
     return render(
         request,
