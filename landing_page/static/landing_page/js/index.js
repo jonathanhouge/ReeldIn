13871fe86d2af35f8971e8bcd6cea757642d2467
Cooper Harris <cwharris365@gmail.com>
2024-04-15 17:52:11 -0700

    Added direct link to movie.html

--- landing_page/static/landing_page/js/index.js
+++ landing_page/static/landing_page/js/index.js
@@ -46,7 +46,7 @@ async function searchMovies(event) {
                 const movieDiv = document.createElement('div');
                 movieDiv.classList.add('searched_movie');
                 movieDiv.onclick = () => {
-                    window.location.href = window.location.origin + "/search/movies?query=" + encodeURIComponent(movie.name)
+                    window.location.href = window.location.origin + "/movie?movie=" + encodeURIComponent(JSON.stringify(movie));
                 }
 
                 // Set the content of the movieDiv
