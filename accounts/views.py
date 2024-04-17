28ffa1de242c33d63cb2098288a969b690ba89e5
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2024-04-17 16:41:53 -0700

    User account creation/sign out

--- accounts/views.py
+++ accounts/views.py
@@ -7,24 +7,27 @@ from .models import User
 
 
 def login_view(request):
-
+    """
+    This function handles the POST login request and the GET login request.
+    POST attempts to login with the provided credentials while GET simply renders the login page.
+    """
     # prevents access by logged in users
     if request.user.is_authenticated:
-        return redirect("dashboard:index")
+        return redirect("landing_page:index")
 
     if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
+        providedUsername = request.POST["username"]
+        providedPassword = request.POST["password"]
+        user = authenticate(request, username=providedUsername, password=providedPassword)
 
         if user is not None:
             login(request, user)
-            return redirect("dashboard:index")
+            return redirect("landing_page:index")
         else:
             return render(
                 request,
                 "accounts/login.html",
-                {"message": "Invalid username and/or password"},
+                {"signInError": "Invalid username and/or password"},
             )
     elif request.method == "GET":
         return render(request, "accounts/login.html")
@@ -32,52 +35,65 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect("accounts:login")
+    return redirect("landing_page:index")
 
 
-def register_view(request):
-
+def signup(request):
+    """
+    This function handles the POST signup request and the GET signup request.
+    POST attempts to create a new user with the provided credentials while 
+    GET simply renders the login page.
+    """
     # prevents access by logged in users
     if request.user.is_authenticated:
-        return redirect("dashboard:index")
+        return redirect("landing_page:index")
 
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
+        # If signup valid, log user in and redirect to onboarding
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("dashboard:index")
+            #request.session["onboarding"] = True TODO link to onboarding later
+            #return render(request, "accounts/onboarding.html")
+            return redirect("landing_page:index")
+        # Else, determine error in signup and return message
         else:
             # checks if username is taken
-            username = request.POST["username"]
-            if User.objects.filter(username=username).exists():
+            providedUsername = request.POST["username"]
+            if User.objects.filter(username=providedUsername).exists():
                 return render(
                     request,
-                    "accounts/register.html",
-                    {"form": form, "message": "Username has already been taken"},
+                    "accounts/login.html",
+                    {"form": form, "signUpError": "Username has already been taken"},
                 )
             # checks if email is taken
-            email = request.POST["email"]
-            if User.objects.filter(email=email).exists():
+            providedEmail = request.POST["email"]
+            if User.objects.filter(email=providedEmail).exists():
                 return render(
                     request,
-                    "accounts/register.html",
-                    {"form": form, "message": "Email address is already in use"},
+                    "accounts/login.html",
+                    {"form": form, "signUpError": "Email address is already in use"},
                 )
+            # checks if passwords match
             password1 = request.POST["password1"]
             confirm = request.POST["password2"]
             if password1 != confirm:
                 return render(
                     request,
-                    "accounts/register.html",
-                    {"form": form, "message": "Passwords do not match"},
+                    "accounts/login.html",
+                    {"form": form, "signUpError": "Passwords do not match"},
                 )
-
+            # if all else fails, return unknown error
             return render(
                 request,
-                "accounts/register.html",
-                {"form": form, "message": "An unknown error occured."},
+                "accounts/login.html",
+                {"form": form, "signUpError": "An unknown error occured."},
             )
     elif request.method == "GET":
         form = CustomUserCreationForm()
-        return render(request, "accounts/register.html", {"form": form})
+        return render(request, "accounts/login.html", {"form": form})
+    
+def onboarding(request):
+    #if(request.session.get("onboarding")):
+    return render(request, "accounts/onboarding.html")
