9ae1e6d8d1b6a3d7373c14d278c487dc7f3f73e4
jonathanhouge <jonathandhouge@gmail.com>
2024-04-19 13:00:18 -0700

    migrations for recommendations and accounts - made a friend request model and more preferences / movie connections

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from recommendations.models import Movie
+from recommendations.choices import GENRES, TRIGGERS
+from django.contrib.postgres.fields import ArrayField
 
 
-# Create your models here.
 class User(AbstractUser):
     username = models.CharField(max_length=16, unique=True)
     password = models.CharField(max_length=100)  # to account for salting & hashing
@@ -16,8 +17,35 @@ class User(AbstractUser):
     )
     imdb_user_id = models.CharField(max_length=32, unique=True, null=True, blank=True)
 
-    # 'Movie' connections - movies already seen and movies already recommended
+    # 'Movie' connections
     seen_films = models.ManyToManyField(Movie, related_name="users_seen", blank=True)
     recommended_films = models.ManyToManyField(
         Movie, related_name="users_recommended", blank=True
+    )
+    watchlist_films = models.ManyToManyField(
+        Movie, related_name="users_watchlisted", blank=True
+    )
+    liked_films = models.ManyToManyField(Movie, related_name="users_liked", blank=True)
+    disliked_films = models.ManyToManyField(
+        Movie, related_name="users_disliked", blank=True
+    )
+
+    # preferences
+    preferred_genres = ArrayField(
+        models.CharField(max_length=13, choices=GENRES), null=True, blank=True
+    )
+    preferred_triggers = ArrayField(
+        models.CharField(max_length=100, choices=TRIGGERS), null=True, blank=True
+    )
+    preferred_cast_and_crew = ArrayField(
+        models.CharField(max_length=100), null=True, blank=True
+    )
+
+
+class FriendRequest(models.Model):
+    sender = models.ForeignKey(
+        "User", related_name="from_user", on_delete=models.CASCADE
+    )
+    receipent = models.ForeignKey(
+        "User", related_name="to_user", on_delete=models.CASCADE
     )
