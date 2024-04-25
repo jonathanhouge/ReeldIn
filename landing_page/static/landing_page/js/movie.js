76667831269b64e8381c807f37d50ab2b08e904e
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-04-25 11:13:50 -0700

    new look update

--- landing_page/static/landing_page/js/movie.js
+++ landing_page/static/landing_page/js/movie.js
@@ -1,25 +1,17 @@
 
 // Buttons for the movie page's 
 // context change depending on button clicked, testing visuals
-function movieOverview(){
+function movieOverview(movie_name, movie_overview){
     document.getElementById("movie_summary").innerHTML = 
-    `Description of <b>Dark Knight</b> Movie: Movie Summary/Plot
-    `;
+    `<p>Description of <strong>${movie_name}</strong> Movie: 
+    ${movie_overview} </p>`;
 }
-function movieDetails(){
+function movieDetails(movie_director, movie_year, movie_runtime){
     document.getElementById("movie_summary").innerHTML = 
-    `<div>Director(s):
-    </div>
-    <div> 
-    Writer(s):
-    </div>
-    <div>
-    Star(s):
-    </div>
-    <div>
-    Genre(s):
-    </div>
-    `;
+    `<p><strong>Director:</strong> ${movie_director}</p>
+    <p><strong>Movie Release Year:</strong> ${movie_year} </p>
+    <p><strong>Movie Runtime:</strong> ${movie_runtime} minutes</p>
+    <p><strong>Genre(s):</strong> </p>`;
 }
 // movie available to watch 
 function movieWatching(){
