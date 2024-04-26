08625239e8bcaa931c1d543cdf01b09ffe732f60
ElizabethODD <elizabethwuhong9@arizona.edu>
2024-04-25 20:37:50 -0700

    movie.html posters updated, movie.html genres added, and some styling

--- landing_page/static/landing_page/js/movie.js
+++ landing_page/static/landing_page/js/movie.js
@@ -3,15 +3,15 @@
 // context change depending on button clicked, testing visuals
 function movieOverview(movie_name, movie_overview){
     document.getElementById("movie_summary").innerHTML = 
-    `<p>Description of <strong>${movie_name}</strong> Movie: 
-    ${movie_overview} </p>`;
+    `<p>Description of <strong>${movie_name}</strong> Movie: </p>
+    <p> ${movie_overview} </p>`;
 }
-function movieDetails(movie_director, movie_year, movie_runtime){
+function movieDetails(movie_director, movie_year, movie_runtime, movie_genres1, movie_genres2, movie_genres3){
     document.getElementById("movie_summary").innerHTML = 
     `<p><strong>Director:</strong> ${movie_director}</p>
     <p><strong>Movie Release Year:</strong> ${movie_year} </p>
     <p><strong>Movie Runtime:</strong> ${movie_runtime} minutes</p>
-    <p><strong>Genre(s):</strong> </p>`;
+    <p><strong>Genres:</strong> ${movie_genres1}, ${movie_genres2}, ${movie_genres3} </p>`;
 }
 // movie available to watch 
 function movieWatching(){
@@ -27,10 +27,10 @@ function movieWatching(){
 function movieOptions(){
     document.getElementById("movie_summary").innerHTML = 
     `<div>
-    Movie Comment(s): 
+    Movie Comments: 
     </div>
     <div>
-    Movie Rating(s):
+    Movie Ratings:
     </div>
     `;
 }
