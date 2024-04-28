5e4d9a2e364cc74a333ceb32be4f5502cad02cd4
farrahirodriguez <102585852+farrahirodriguez@users.noreply.github.com>
2024-04-28 00:15:18 -0700

    fixed landing page footer and removed redundant comment

--- landing_page/static/landing_page/js/movie.js
+++ landing_page/static/landing_page/js/movie.js
@@ -1,5 +1,4 @@
 // Buttons for the movie page's
-// Buttons for the movie page's
 // context change depending on button clicked, testing visuals
 function movieOverview(movie_data) {
   console.log("movie_data: ", movie_data);
