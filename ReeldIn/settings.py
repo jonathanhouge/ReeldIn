3be60df83eac2264f337c6565a81fd9650fa55c3
Cooper Harris <cwharris365@gmail.com>
2024-04-28 18:40:18 -0700

    re adding whitenoise middleware

--- ReeldIn/settings.py
+++ ReeldIn/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django_browser_reload.middleware.BrowserReloadMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
