85b8b2dfe2619d6ef29ded316ab68639e8dfa045
Julram3232 <48529138+NerdRez@users.noreply.github.com>
2024-04-29 18:13:46 -0700

    More mobile styling, fixed home rerouting

--- accounts/static/accounts/js/onboarding_triggers.js
+++ accounts/static/accounts/js/onboarding_triggers.js
@@ -5,13 +5,3 @@ function redirectToOnboardingMovieForm() {
 function submitTriggerForm() {
   document.getElementById("onboarding-form").submit();
 }
-
-function openBackModal() {
-  document.getElementById("confirm_back_modal").classList.remove("hidden");
-  document.getElementById("exit_overlay").classList.remove("hidden");
-}
-
-function closeBackModal() {
-  document.getElementById("confirm_back_modal").classList.add("hidden");
-  document.getElementById("exit_overlay").classList.add("hidden");
-}
