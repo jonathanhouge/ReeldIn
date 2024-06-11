2a10c097b12a9fc0509e5c17e4435168aa5771ae
jonathanhouge <jonathandhouge@gmail.com>
2024-06-11 11:08:52 -0700

    julius wanted line 22 to change

--- landing_page/static/landing_page/js/index.js
+++ landing_page/static/landing_page/js/index.js
@@ -1,117 +1,118 @@
 async function getCSRFToken() {
-    try {
-        const response = await fetch("/get-csrf-token/");
-        const data = await response.json();
-        const csrfToken = data.csrf_token;
+  try {
+    const response = await fetch("/get-csrf-token/");
+    const data = await response.json();
+    const csrfToken = data.csrf_token;
 
-        return csrfToken;
-    } catch (error) {
-        console.error("Error fetching CSRF token:", error);
-        throw error; // Rethrow the error to propagate it
-    }
+    return csrfToken;
+  } catch (error) {
+    console.error("Error fetching CSRF token:", error);
+    throw error; // Rethrow the error to propagate it
+  }
 }
 
-
 async function searchMovies(event) {
-    searchBar = document.getElementById("searchbar")
-    searchString = searchbar.value.trim()
+  searchBar = document.getElementById("searchbar");
+  searchString = searchbar.value.trim();
 
-    // Enter key
-    if ((event.key === "Enter" || event.type === "click") && searchString) {
-        // Route to search movies
-        window.location.href = "/search/movies?query=" + encodeURIComponent(searchString) + "/"
-    } else if (searchString) {
-        const csrfToken = await getCSRFToken(); // Wait until getCSRFToken() resolves
-        fetch("/api/search/movies/", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRFToken": csrfToken
-            },
-            body: JSON.stringify({ search: searchString})
-        })
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok");
-            }
-            return response.json()
-        })
-        .then((data) => {
-            moviesContainer = document.getElementById("searched_movies_container")
-            moviesContainer.innerHTML = '';
-            searchbar = document.getElementById("searchbar")
-            searchbar.style.borderRadius = "12px 12px 0 0"
+  // Enter key
+  if ((event.key === "Enter" || event.type === "click") && searchString) {
+    // Route to search movies
+    window.location.href =
+      "/search/movies?query=" + encodeURIComponent(searchString);
+  } else if (searchString) {
+    const csrfToken = await getCSRFToken(); // Wait until getCSRFToken() resolves
+    fetch("/api/search/movies/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrfToken,
+      },
+      body: JSON.stringify({ search: searchString }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        moviesContainer = document.getElementById("searched_movies_container");
+        moviesContainer.innerHTML = "";
+        searchbar = document.getElementById("searchbar");
+        searchbar.style.borderRadius = "12px 12px 0 0";
 
-            data.movies.forEach((movie, index) => {
-                // Create a new <div> element to display the movie details
-                const movieDiv = document.createElement('div');
-                movieDiv.classList.add('searched_movie');
-                movieDiv.onclick = () => {
-                    window.location.href = window.location.origin + `/movie/${movie.id}`;
-                }
+        data.movies.forEach((movie, index) => {
+          // Create a new <div> element to display the movie details
+          const movieDiv = document.createElement("div");
+          movieDiv.classList.add("searched_movie");
+          movieDiv.onclick = () => {
+            window.location.href =
+              window.location.origin + `/movie/${movie.id}`;
+          };
 
-                // Set the content of the movieDiv
-                var img = document.createElement('img');
-                img.classList.add('suggestions_img')
+          // Set the content of the movieDiv
+          var img = document.createElement("img");
+          img.classList.add("suggestions_img");
 
-                img.id = `${movie.id}`;
-                img.src = `https://image.tmdb.org/t/p/h100${movie.poster}`;
+          img.id = `${movie.id}`;
+          img.src = `https://image.tmdb.org/t/p/h100${movie.poster}`;
 
-                var textDiv = document.createElement('div');
-                textDiv.classList.add('searched_movie_details')
+          var textDiv = document.createElement("div");
+          textDiv.classList.add("searched_movie_details");
 
-                var title = document.createElement('h3');
-                title.innerHTML = `${movie.name}`;
+          var title = document.createElement("h3");
+          title.innerHTML = `${movie.name}`;
 
-                var release_date = document.createElement('p');
-                release_date.innerHTML = `${movie.year}`;
+          var release_date = document.createElement("p");
+          release_date.innerHTML = `${movie.year}`;
 
-                var starring = document.createElement('p');
-                starring.innerHTML = `${movie.starring.slice(0, 2).join(', ')}`;
+          var starring = document.createElement("p");
+          starring.innerHTML = `${movie.starring.slice(0, 2).join(", ")}`;
 
-                textDiv.appendChild(title);
-                textDiv.appendChild(release_date);
-                textDiv.appendChild(starring);
-                
-                movieDiv.appendChild(img)
-                movieDiv.appendChild(textDiv);
+          textDiv.appendChild(title);
+          textDiv.appendChild(release_date);
+          textDiv.appendChild(starring);
 
-                // Check if it's the last iteration
-                if (index === data.movies.length - 1) {
-                    movieDiv.classList.add('last_movie');
-                }
+          movieDiv.appendChild(img);
+          movieDiv.appendChild(textDiv);
 
-                // Append the movieDiv to the moviesContainer
-                moviesContainer.appendChild(movieDiv);
-            });
-        })
-        .catch((error) => {
-            console.error("Error:", error);
+          // Check if it's the last iteration
+          if (index === data.movies.length - 1) {
+            movieDiv.classList.add("last_movie");
+          }
+
+          // Append the movieDiv to the moviesContainer
+          moviesContainer.appendChild(movieDiv);
         });
-    } else {
-        moviesContainer = document.getElementById("searched_movies_container")
-        moviesContainer.innerHTML = '';
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    moviesContainer = document.getElementById("searched_movies_container");
+    moviesContainer.innerHTML = "";
 
-        searchbar = document.getElementById("searchbar")
-            searchbar.style.borderRadius = "12px"
-    }
+    searchbar = document.getElementById("searchbar");
+    searchbar.style.borderRadius = "12px";
+  }
 }
 
 function hideSearchResults() {
-    moviesContainer = document.getElementById("searched_movies_container")
-    moviesContainer.classList.add('hidden');
+  moviesContainer = document.getElementById("searched_movies_container");
+  moviesContainer.classList.add("hidden");
 
-    searchbar = document.getElementById("searchbar")
-    searchbar.style.borderRadius = "12px"
+  searchbar = document.getElementById("searchbar");
+  searchbar.style.borderRadius = "12px";
 }
 
 function showSearchResults() {
-    moviesContainer = document.getElementById("searched_movies_container")
-    moviesContainer.classList.remove('hidden');
+  moviesContainer = document.getElementById("searched_movies_container");
+  moviesContainer.classList.remove("hidden");
 
-    searchbar = document.getElementById("searchbar")
+  searchbar = document.getElementById("searchbar");
 
-    if (searchbar.value.trim() !== '') {
-        searchbar.style.borderRadius = "12px 12px 0 0"
-    }
+  if (searchbar.value.trim() !== "") {
+    searchbar.style.borderRadius = "12px 12px 0 0";
+  }
 }
